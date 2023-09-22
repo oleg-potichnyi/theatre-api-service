146d1c102eba50231865b774e05ab3a128b74ef5
oleg-potichnyi <potichnyi.oleg@gmail.com>
2023-09-22 18:23:33 +0300

    Added model Genre

--- theatre/models.py
+++ theatre/models.py
@@ -12,3 +12,10 @@ class TheatreHall(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name

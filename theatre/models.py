f8c9874c4733e3878d4eef7e41f2ff8a6ac04cff
oleg-potichnyi <potichnyi.oleg@gmail.com>
2023-09-22 18:20:46 +0300

    Added model TheatreHall

--- theatre/models.py
+++ theatre/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class TheatreHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+    def __str__(self):
+        return self.name

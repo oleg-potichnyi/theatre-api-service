c67ea3b4a1c704e0363a1c63f12e349c0ae5545b
oleg-potichnyi <potichnyi.oleg@gmail.com>
2023-09-22 18:28:00 +0300

    Added model Actor

--- theatre/models.py
+++ theatre/models.py
@@ -19,3 +19,15 @@ class Genre(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"

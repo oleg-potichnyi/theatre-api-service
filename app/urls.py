05ebbcd1654578ac0a39df4e9e88912893d79ae0
oleg-potichnyi <potichnyi.oleg@gmail.com>
2023-09-23 17:25:58 +0300

    Created app 'user'

--- app/urls.py
+++ app/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("api/theatre/", include("theatre.urls", namespace="theatre")),
     path("api/user/", include("user.urls", namespace="user")),
     path("__debug__/", include("debug_toolbar.urls")),

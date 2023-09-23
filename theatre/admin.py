44371292459e102442c2a153bb7123378bbb0218
oleg-potichnyi <potichnyi.oleg@gmail.com>
2023-09-23 13:31:37 +0300

    Models are registered in the admin.py file

--- theatre/admin.py
+++ theatre/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from theatre.models import (
+    TheatreHall,
+    Genre,
+    Actor,
+    Play,
+    Performance,
+    Reservation,
+    Ticket,
+)
+
+admin.site.register(TheatreHall)
+admin.site.register(Genre)
+admin.site.register(Actor)
+admin.site.register(Play)
+admin.site.register(Performance)
+admin.site.register(Reservation)
+admin.site.register(Ticket)

bc555b17c2b92f242a1cab0237829e4fd16ce961
oleg-potichnyi <potichnyi.oleg@gmail.com>
2023-09-23 14:06:59 +0300

    In file url.py defined the URL patterns and routes for the project.

--- app/urls.py
+++ app/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path("api/theatre/", include("theatre.urls", namespace="theatre")),
+    path("api/user/", include("user.urls", namespace="user")),
+    path("__debug__/", include("debug_toolbar.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

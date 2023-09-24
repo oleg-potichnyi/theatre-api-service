7d2acc8a0deee3b7ee6c79e0d404a53d4221cae2
oleg-potichnyi <potichnyi.oleg@gmail.com>
2023-09-24 18:02:30 +0300

    Added custom permission class IsAdminOrIfAuthenticatedReadOnly

--- theatre/views.py
+++ theatre/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
-# Create your views here.
+# from rest_framework import mixins
+# from rest_framework.viewsets import GenericViewSet
+#
+# from theatre.models import Genre
+# from theatre.serializers import (
+#     GenreSerializer,
+# )
+#
+#
+# class GenreViewSet(
+#     mixins.CreateModelMixin,
+#     mixins.ListModelMixin,
+#     GenericViewSet,
+# ):
+#     queryset = Genre.objects.all()
+#     serializer_class = GenreSerializer
+#     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)

864c47dfe6e8e6e621961d04b095a85139f5c72b
oleg-potichnyi <potichnyi.oleg@gmail.com>
2023-09-24 19:42:36 +0300

    Created urlpatterns for theatre app

--- app/urls.py
+++ app/urls.py
@@ -18,10 +18,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/theatre/", include(("theatre.urls", "theatre"), namespace="theatre")),
     path("api/user/", include("user.urls", namespace="user")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
     path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

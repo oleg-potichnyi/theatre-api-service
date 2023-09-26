7c5f65258a054d3eb976f8efba6712c3d765cac2
oleg-potichnyi <potichnyi.oleg@gmail.com>
2023-09-26 15:46:15 +0300

    Added files .env and .env.sample

--- app/settings.py
+++ app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-u5w2jfmizo$*z41&jwv%g7-v&h)jxmiboto)3l39&_od!&@)%3"
+SECRET_KEY = os.environ.get("SECRET_KEY", default="your secret key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
